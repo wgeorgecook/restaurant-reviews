56e7b55ce18b88c2e3183d6c1bc244f168c29d48
William George Cook <wgeorgecook@gmail.com>
2018-09-03 11:18:36 -0700

    Service worker fails to register

--- js/main.js
+++ js/main.js
@@ -4,6 +4,13 @@ let restaurants,
 var newMap
 var markers = []
 
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('../sw.js');
+  });
+}
+
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
--- js/sw.js
+++ js/sw.js
@@ -1,1 +1,64 @@
 // Service worker file
+
+var staticCacheName = 'restaurant-static-v1';
+
+var imgCache = [
+    'img/1.jpg',
+    'img/2.jpg',
+    'img/3.jpg',
+    'img/4.jpg',
+    'img/5.jpg',
+    'img/6.jpg',
+    'img/7.jpg',
+    'img/8.jpg',
+    'img/9.jpg',
+    'img/10.jpg'
+]
+var toCache = [
+    '/',
+    'js/dbhelper.js',
+    'js/main.js',
+    'js/restaurant_helper.js',
+    'css/styles.css',
+    imgCache,
+  ]
+
+self.addEventListener('install', function(event) {
+
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll(toCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('restaurant-') &&
+                 cacheName != staticCacheName;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch('/');
+    })
+  );
+});
+
+self.addEventListener('message', function(event) {
+  if (event.data.action === 'skipWaiting') {
+    console.log("Page refresh coming");
+    self.skipWaiting();
+  }
+});
