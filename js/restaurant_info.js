62b3a1748cd10e49661a344c473e88a83a924001
William George Cook <wgeorgecook@gmail.com>
2018-09-03 11:59:27 -0700

    Restaurant detail page now has descriptive alt captions

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -88,7 +88,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
 
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img';
-  image.alt = restaurant.alt};
+  image.alt = restaurant.alt;
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
 
   const cuisine = document.getElementById('restaurant-cuisine');
