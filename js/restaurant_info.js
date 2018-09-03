0eb17059b38df674b5803d4855147b149bdf2d30
William George Cook <wgeorgecook@gmail.com>
2018-09-02 20:00:42 -0700

    Programatically add alt tag to images

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -87,7 +87,8 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   address.innerHTML = restaurant.address;
 
   const image = document.getElementById('restaurant-img');
-  image.className = 'restaurant-img'
+  image.className = 'restaurant-img';
+  image.alt = `photo of ${restaurant.name}`;
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
 
   const cuisine = document.getElementById('restaurant-cuisine');
