d086d1bdaea919fed804601e6f16ab0fa82adce4
William George Cook <wgeorgecook@gmail.com>
2018-09-03 11:49:34 -0700

    Alt for each image in json file.

--- js/main.js
+++ js/main.js
@@ -161,7 +161,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = `photo of ${restaurant.name}`;
+  image.alt = restaurant.alt;
   li.append(image);
 
   const name = document.createElement('h1');
