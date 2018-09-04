5110ffb70fac9ca2dc2f642ad07b5b63760b0ce1
William George Cook <wgeorgecook@gmail.com>
2018-09-03 18:59:03 -0700

    Cache visited pages or serve offline unavailable page if no cached asset available

--- sw.js
+++ sw.js
@@ -9,31 +9,38 @@ self.addEventListener('install', function(event) {
     caches.open('restaurant-cache').then(function(cache) {
       return cache.addAll([
         '/',
+        '/offline.html',
         '/restaurant.html',
-        'dbhelper.js',
-        'main.js',
-        'restaurant_info.js',
-        '../css/styles.css',
-        '../img/1.jpg',
-        '../img/2.jpg',
-        '../img/3.jpg',
-        '../img/4.jpg',
-        '../img/5.jpg',
-        '../img/6.jpg',
-        '../img/7.jpg',
-        '../img/8.jpg',
-        '../img/9.jpg',
-        '../img/10.jpg',
+        'js/dbhelper.js',
+        'js/main.js',
+        'js/restaurant_info.js',
+        '/css/styles.css',
+        'img/1.jpg',
+        'img/2.jpg',
+        'img/3.jpg',
+        'img/4.jpg',
+        'img/5.jpg',
+        'img/6.jpg',
+        'img/7.jpg',
+        'img/8.jpg',
+        'img/9.jpg',
+        'img/10.jpg',
       ]);
     })
   );
 });
 
+// Thanks to the Google Developers Tutorials for most of this code snippet!
+// https://developers.google.com/web/ilt/pwa/caching-files-with-service-worker
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      console.log(response);
-      return response || fetch(event.request);
+    caches.open('restaurant-cache').then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response
+        }).catch(function() {return cache.match('/offline.html')});
+      });
     })
   );
 });
